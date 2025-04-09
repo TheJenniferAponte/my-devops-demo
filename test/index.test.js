8f87638462c8a8bef0a226a0e7c7d944e4efd9dc
Jennifer <jenniferoaponte@gmail.com>
2025-04-08 20:50:22 -0500

    Update test to match new app message

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,11 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  it('should return Hello, Docker CI/CD!', (done) => {
+  it('should return Hello, Updated Docker CI/CD!', (done) => {
     request(app)
       .get('/')
       .expect(200)
-      .expect('Hello, Docker CI/CD!')
+      .expect('Hello, Updated Docker CI/CD!')
       .end(done);
   });
 });
